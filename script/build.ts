4e8f9a2659bf306a49569d8d7ffde31d63f26d29
macbook <macbook@m2s-MacBook-Air.local>
2026-05-17 19:36:16 +0500

    Revert "feat: replace Tesseract OCR with PaddleOCR PP-OCRv5 Mobile models"
    This reverts commit de4b02d19d90ce12879a50926f16472e517bfd37.

--- script/build.ts
+++ script/build.ts
@@ -1,6 +1,6 @@
 import { build as esbuild } from "esbuild";
 import { build as viteBuild } from "vite";
-import { rm, readFile, copyFile } from "fs/promises";
+import { rm, readFile } from "fs/promises";
 
 // server deps to bundle to reduce openat(2) syscalls
 // which helps cold start times
@@ -59,10 +59,6 @@ async function buildAll() {
     external: externals,
     logLevel: "info",
   });
-
-  // Copy PaddleOCR Python worker to dist/ (referenced via __dirname at runtime)
-  console.log("copying PaddleOCR worker...");
-  await copyFile("server/paddle-ocr-worker.py", "dist/paddle-ocr-worker.py");
 }
 
 buildAll().catch((err) => {
